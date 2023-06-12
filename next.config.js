7c71076b5774fedebdf5c89918fc1bc8e3e630e1
Johnny Squardo <johnnysquardo@gmail.com>
2023-06-12 17:46:28 -0400

    Feature: Start implementing MDX

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { withContentlayer } from "next-contentlayer";
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = withContentlayer({
+  pageExtensions: ["ts", "tsx", "md", "mdx"],
+});
