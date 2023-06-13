34ef7f95b31f5d78cf9941cec4249b94519d8071
Johnny Squardo <johnnysquardo@gmail.com>
2023-06-13 18:25:24 -0400

    Reverted to working state

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-import { withContentlayer } from "next-contentlayer";
 const nextConfig = {};
 
-module.exports = withContentlayer({
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-});
+module.exports = nextConfig;
