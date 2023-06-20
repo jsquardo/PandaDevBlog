113d037cb752fc022b86f7d22c2b962916f3d667
Johnny Squardo <johnnysquardo@gmail.com>
2023-06-20 19:04:17 -0400

    ADD: api

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,64 +1,100 @@
+import { BlogDate } from "@/components/BlogDate";
+
+import { getTweets } from "lib/twitter";
+import { allPosts } from "../../../.contentlayer/generated";
 import { notFound } from "next/navigation";
-import { allPosts } from "contentlayer/generated";
+import { Mdx } from "../../../components/mdx-components";
 
-import { Metadata } from "next";
-import { Mdx } from "@/components/mdx-components";
-
-interface PostProps {
-  params: {
-    slug: string[];
-  };
-}
-
-async function getPostFromParams(params: PostProps["params"]) {
-  const slug = params?.slug?.join("/");
-  const post = allPosts.find((post) => post.slugAsParams === slug);
-
-  if (!post) {
-    null;
-  }
-
-  return post;
-}
-
-export async function generateMetadata({
-  params,
-}: PostProps): Promise<Metadata> {
-  const post = await getPostFromParams(params);
-
-  if (!post) {
-    return {};
-  }
-
-  return {
-    title: post.title,
-    description: post.description,
-  };
-}
-
-export async function generateStaticParams(): Promise<PostProps["params"][]> {
-  return allPosts.map((post) => ({
-    slug: post.slugAsParams.split("/"),
-  }));
-}
-
-export default async function PostPage({ params }: PostProps) {
-  const post = await getPostFromParams(params);
+export default async function Post({ params }) {
+  const post = allPosts.find((post) => post.slug === params.slug);
 
   if (!post) {
     notFound();
   }
 
+  const tweets = await getTweets(post.tweetIds);
+
+  const canonicalUrl = `https://pandadev.dev/posts/${post.slug}`;
+  const GITHUB_USERNAME = "jsquardo";
+  const GITHUB_REPO = "pandadev.dev";
+  const GITHUB_URL = `https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO}/edit/master/content/posts/${post.slug}.mdx`;
+  const TWITTER_URL = `https://twitter.com/search?q=${encodeURIComponent(
+    canonicalUrl
+  )}`;
+
   return (
-    <article className="py-6 prose dark:prose-invert">
-      <h1 className="mb-2">{post.title}</h1>
-      {post.description && (
-        <p className="text-xl mt-0 text-slate-700 dark:text-slate-200">
-          {post.description}
+    <>
+      <main>
+        <h1 className="mb-7 mt-10 sm:text-4xl text-3xl font-bold font-serif">
+          {post.title}
+        </h1>
+        <p className="leading-7 mb-7 -mt-6">
+          <BlogDate date={post.date} minutes={post.readTime} />
         </p>
-      )}
-      <hr className="my-4" />
-      <Mdx code={post.body.code} />
-    </article>
+        <article className="prose dark:prose-dark w-full">
+          <Mdx post={post} tweets={tweets} />
+        </article>
+        <footer className="mb-8 mt-6">
+          <p className="text-base">
+            <a
+              className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none"
+              href={TWITTER_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Discuss on Twitter
+            </a>{" "}
+            &bull;{" "}
+            <a
+              className="text-pink-700 dark:text-pink-300 shadow-link hover:shadow-none"
+              href={GITHUB_URL}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Edit on Github
+            </a>
+          </p>
+        </footer>
+      </main>
+      <Aside />
+      <Pagination page={post.slug} />
+    </>
   );
+}
+
+export async function generateStaticParams() {
+  return allPosts.map((post) => ({
+    params: {
+      slug: post.slug,
+    },
+  }));
+}
+
+export async function generateMetadata({ params }) {
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  const ogImage = `https://pandadev.dev/api/og?title=${post?.title}`;
+
+  return {
+    title: `${post?.title} - Johnny Squardo`,
+    description: post?.excerpt,
+    openGraph: {
+      title: post?.title,
+      type: "website",
+      siteName: "Johnny Squardo",
+      description: post?.excerpt,
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      site: "@pandadevv",
+      title: post?.title,
+      description: post?.excerpt,
+      images: [ogImage],
+    },
+  };
 }
